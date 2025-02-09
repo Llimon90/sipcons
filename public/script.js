0df3fc37de302d2961aae4d2730bce84168b2d77
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-09 00:52:57 -0800

    se cambia user y pass de bd

--- public/script.js
+++ public/script.js
@@ -1,33 +1,33 @@
 document.getElementById('new-incidencia-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenir el envío por defecto del formulario
-    console.log('Formulario enviado');
+  event.preventDefault(); // Prevenir el envío por defecto del formulario
+  console.log('Formulario enviado');
   
-    const nuevaIncidencia = {
-      numero: document.getElementById('numero').value,
-      cliente: document.getElementById('cliente').value,
-      contacto: document.getElementById('contacto').value,
-      sucursal: document.getElementById('sucursal').value,
-      falla: document.getElementById('falla').value,
-      fecha: document.getElementById('fecha').value,
-      tecnico: document.getElementById('tecnico').value,
-      status: document.getElementById('status').value
-    };
-  
-    fetch('http://localhost:3000/nueva-incidencia', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(nuevaIncidencia)
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Respuesta del servidor:', data);
-    })
-    .catch(error => {
-      console.error('Error al enviar los datos:', error);
-    });
+  const nuevaIncidencia = {
+    cliente: document.getElementById('cliente').value,
+    contacto: document.getElementById('contacto').value,
+    sucursal: document.getElementById('sucursal').value,
+    falla: document.getElementById('falla').value,
+    fecha: document.getElementById('fecha').value,
+    tecnico: document.getElementById('tecnico').value,
+    status: document.getElementById('status').value
+  };
+
+  fetch('https://tudominio.com/api.php', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(nuevaIncidencia)
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Respuesta del servidor:', data);
+    alert(data.message); // Mostrar un mensaje de éxito o error
+  })
+  .catch(error => {
+    console.error('Error al enviar los datos:', error);
+    alert('Hubo un error al enviar los datos');
   });
-  
-  //INSERTA CONSECUTIVO DE INCIDENCIA EN FORMULARIO
-  
+});
+
+//INSERTA CONSECUTIVO DE INCIDENCIA EN FORMULARIO
