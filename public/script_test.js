760d270c71fe5000fd2c3fbdb92f26c6430d9699
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-21 19:49:43 -0800

    se CORRIGE ESTRUCTURA DE ARCHIVOS SE AGREGAN DIRECTORIOS BACKEND Y SCRIPTS

--- public/script_test.js
+++ public/script_test.js
@@ -53,12 +53,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 tbody.innerHTML = ""; // Limpia la tabla antes de agregar nuevas filas
 
                 if (data.error) {
-                    tbody.innerHTML = `<tr><td colspan="9">${data.error}</td></tr>`;
+                    tbody.innerHTML = `<tr><td colspan="30">${data.error}</td></tr>`;
                     return;
                 }
 
                 if (data.message) {
-                    tbody.innerHTML = `<tr><td colspan="9">${data.message}</td></tr>`;
+                    tbody.innerHTML = `<tr><td colspan="30">${data.message}</td></tr>`;
                     return;
                 }
 
