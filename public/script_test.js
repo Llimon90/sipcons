b45eebe2006b28c96fd400fd5137271e32542c58
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-21 19:45:52 -0800

    se CORRIGE ESTRUCTURA DE ARCHIVOS SE AGREGAN DIRECTORIOS BACKEND Y SCRIPTS

--- public/script_test.js
+++ public/script_test.js
@@ -53,7 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 tbody.innerHTML = ""; // Limpia la tabla antes de agregar nuevas filas
 
                 if (data.error) {
-                    tbody.innerHTML = `<tr><td colspan="9">${data.error}</td></tr>`;
+                    tbody.innerHTML = `<tr><td colspan="15">${data.error}</td></tr>`;
                     return;
                 }
 
@@ -68,11 +68,11 @@ document.addEventListener("DOMContentLoaded", function () {
                         <td>${incidencia.numero}</td>
                         <td>${incidencia.cliente}</td>
                         <td>${incidencia.contacto}</td>
-                        <td>${incidencia.sucursal}</td>
+                 
                         <td>${incidencia.falla}</td>
                         <td>${incidencia.fecha}</td>
                         <td>${incidencia.tecnico}</td>
-                        <td>${incidencia.estatus}</td>
+                
                         <td>${incidencia.numero_incidente ?? }</td>
                     `;
                     tbody.appendChild(fila);
