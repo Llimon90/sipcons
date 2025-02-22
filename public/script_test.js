db3e1351647f9560fbc98e0fec28faf3070d5a69
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-21 20:55:26 -0800

    se CORRIGE ESTRUCTURA DE ARCHIVOS SE AGREGAN DIRECTORIOS BACKEND Y SCRIPTS

--- public/script_test.js
+++ public/script_test.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                     const fila = document.createElement("tr");
                     fila.innerHTML = `
-                        <td>${incidencia.id ?? 'N/A'}</td>
+                        <td>${incidencia.id ?? }</td>
                         <td>${incidencia.numero}</td>
                         <td>${incidencia.cliente}</td>
                         <td>${incidencia.contacto}</td>
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         <td>${incidencia.fecha}</td>
                         <td>${incidencia.tecnico}</td>
                         <td>${incidencia.estatus}</td>
-                        <td>${incidencia.numero_incidente ?? 'N/A'}</td>
+                        <td>${incidencia.numero_incidente ?? }</td>
 
                         
                     `;
