63e4576cdbbd88bb3c0412b7b96585db85c4a16b
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-26 15:26:11 -0800

    se corrige error de script_cargas error en innerHTML

--- public/script_cargas.js
+++ public/script_cargas.js
@@ -1,44 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     function cargarIncidencias() {
-//         fetch("server.php")
-//             .then(response => response.json())
-//             .then(data => {
-//                 const tbody = document.getElementById("tabla-body");
-//                 // tbody.innerHTML = "fila"; // Limpiar tabla antes de agregar nuevas filas
-
-//                 if (data.error) {
-//                     // tbody.innerHTML = `<tr><td colspan="8">${data.error}</td></tr>`;
-//                     return;
-//                 }
-
-//                 if (data.message) {
-//                     // tbody.innerHTML = `<tr><td colspan="8">${data.message}</td></tr>`;
-//                     // return;
-//                 }
-
-//                 data.forEach(incidencia => {
-//                     const fila = document.createElement("tr");
-//                     fila.innerHTML = `
-//                         <td>${incidencia.numero}</td>
-                
-//                         <td>${incidencia.cliente}</td>
-//                         <td>${incidencia.contacto}</td>
-//                         <td>${incidencia.sucursal}</td>
-//                         <td>${incidencia.falla}</td>
-//                         <td>${incidencia.fecha}</td>
-//                         <td>${incidencia.tecnico}</td>
-//                         <td>${incidencia.estatus}</td>
-                        
-//                     `;
-//                     tbody.appendChild(fila);
-//                 });
-//             })
-//             .catch(error => console.error("Error al cargar incidencias:", error));
-//     }
-
-//     cargarIncidencias(); // Llamar a la función al cargar la página
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
     function cargarIncidencias() {
         fetch("server.php")
@@ -59,18 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 data.forEach(incidencia => {
                     const fila = document.createElement("tr");
-                    fila.innerHTML = `
-                        <td>${incidencia.numero}</td>
+                    // fila.innerHTML = `
+                    //     <td>${incidencia.numero}</td>
                 
-                        <td>${incidencia.cliente}</td>
-                        <td>${incidencia.contacto}</td>
-                        <td>${incidencia.sucursal}</td>
-                        <td>${incidencia.falla}</td>
-                        <td>${incidencia.fecha}</td>
-                        <td>${incidencia.tecnico}</td>
-                        <td>${incidencia.estatus}</td>
+                    //     <td>${incidencia.cliente}</td>
+                    //     <td>${incidencia.contacto}</td>
+                    //     <td>${incidencia.sucursal}</td>
+                    //     <td>${incidencia.falla}</td>
+                    //     <td>${incidencia.fecha}</td>
+                    //     <td>${incidencia.tecnico}</td>
+                    //     <td>${incidencia.estatus}</td>
                         
-                    `;
+                    // `;
                     tbody.appendChild(fila);
                 });
             })
