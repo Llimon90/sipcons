942ffb2cbfec367f5c45084be3fe07427dfde021
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-26 20:24:56 -0800

    se aplican cambios crude para modificar incidencias

--- public/script_cargas.js
+++ public/script_cargas.js
@@ -33,14 +33,22 @@ document.addEventListener("DOMContentLoaded", function () {
                 incidenciasArray.forEach(incidencia => {
                     const fila = document.createElement("tr");
                     fila.innerHTML = `
-                      <td>${incidencia.numero}</td>
-                        <td>${incidencia.numero_incidente}</td>
+                        <td>${incidencia.numero}</td>
+                        <a href="crude-incidencias.php"><td>${incidencia.numero_incidente}</td></a>
                         <td>${incidencia.cliente}</td>
                         <td>${incidencia.sucursal}</td>
                         <td>${incidencia.falla}</td>
                         <td>${incidencia.fecha}</td>
                         <td>${incidencia.estatus}</td>
                     `;
+
+                    // Añadir evento de doble clic a la celda numero_incidente
+    fila.querySelector(".numero-incidente").addEventListener("dblclick", () => {
+        // Redirigir a la página de edición con el ID de la incidencia
+        window.location.href = `crude-incidencia.php?id=${incidencia.id}`; // Asegúrate de que 'id' esté presente en el objeto 'incidencia'
+    });
+
+
                     tbody.appendChild(fila);
                 });
             })
