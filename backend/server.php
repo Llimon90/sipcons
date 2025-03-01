efde768c9c861e0752e9da868afe23fbb92ae262
Luis_Limon <Luis.limon.iml@gmail.com>
2025-02-28 16:23:09 -0800

    se aplican cambios en crud incidencias

--- backend/server.php
+++ backend/server.php
@@ -27,7 +27,7 @@ if ($method === "GET") {
     // **INICIO - FUNCIÓN PARA MOSTRAR LA BASE DE DATOS EN EL DOM**
     
     // Consulta para obtener todas las incidencias sin filtrar
-    $sql = "SELECT * FROM incidencias WHERE estatus = 'Abierta' 'En seguimineto' 'Pendiente'";
+    $sql = "SELECT * FROM incidencias WHERE estatus = 'Abierta' OR 'En seguimineto' OR 'Pendiente'";
 
 
     $result = $conn->query($sql);
