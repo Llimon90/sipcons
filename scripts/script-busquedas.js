77e7c7b81e23cd845cb0cceb7560db74e6155b20
Luis_Limon <Luis.limon.iml@gmail.com>
2025-03-06 01:20:56 -0800

    trabajo con filtros de busqueda

--- scripts/script-busquedas.js
+++ scripts/script-busquedas.js
@@ -157,15 +157,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-   // Agregar funcionalidad al botón de limpiar filtros
-   document.getElementById('limpiar-filtros').addEventListener('click', function() {
-    // Limpiar todos los filtros
-    document.getElementById('report-form').reset();
-
-    // Recargar la página para limpiar los datos cargados
-    window.location.reload();
-  });
-
+ 
   
   // Cargar las incidencias al cargar la página (sin filtros)
   cargarIncidencias();
