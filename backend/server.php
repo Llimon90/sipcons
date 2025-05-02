a4122c50efa702089027b9aa1fab5a3fbe275385
Llimon90 <luis.limon.iml@gmail.com>
2025-05-02 13:09:32 -0700

    SE ACTUALIZAN ELEMNTOS DE SELEC ESTATUS EN REPORTES.HTML Y EN QUERY DE SERVE.PHP

--- backend/server.php
+++ backend/server.php
@@ -27,7 +27,7 @@ if ($method === "GET") {
     // **INICIO - FUNCIÓN PARA MOSTRAR LA BASE DE DATOS EN EL DOM**
     
     // Consulta para obtener todas las incidencias sin filtrar
-    $sql = "SELECT * FROM incidencias WHERE estatus IN ('Abierta', 'En seguimiento', 'Pendiente')";
+    $sql = "SELECT * FROM incidencias WHERE estatus IN ('Abierto', 'Asignado', 'Cerrado sin factura')";
 
 
 
