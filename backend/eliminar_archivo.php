60c1e177ce68c11d25f510ce916117185569f78f
Llimon90 <luis.limon.iml@gmail.com>
2026-05-28 01:22:24 -0700

    Se agrega proyecto a nuevo entorno de pruebas y a rama nueva de PRUEBAS V1

--- backend/eliminar_archivo.php
+++ backend/eliminar_archivo.php
@@ -1,7 +1,7 @@
 ﻿<?php
 require_once __DIR__ . '/../auth/middleware.php';
 header('Content-Type: application/json');
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // ==============================================
